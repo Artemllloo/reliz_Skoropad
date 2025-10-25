2bda6a058d1d529cc3b102bb5d8e8bf23ab685f6
Artemllloo <iteplitskyilogika@gmail.com>
2025-10-25 13:23:56 +0300

    day 2

---
+++ main.js
@@ -0,0 +1,29 @@
+// Оголошуємо асинхронну функцію для отримання продуктів з сервера
+async function getProducts() {
+    // Виконуємо запит до файлу "store_db.json" та очікуємо на відповідь
+    let response = await fetch("store.json")
+    // Очікуємо на отримання та розпакування JSON-даних з відповіді
+    let products = await response.json()
+    // Повертаємо отримані продукти
+    return products
+};
+
+function getCardHTML(product) {return`<div class="card" style="width: 18rem;">
+  <img src="img/$(product.image)" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">$(product.title)</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`}
+
+
+// Викликаємо асинхронну функцію та очікуємо на отримання продуктів
+getProducts().then(function (products)) 
+    let productsList = document.querySelector('.products-list')
+    if (productsList) {
+        products.forEach(function (product) {
+            // Відображаємо товари на сторінці
+            productsList.innerHTML += getCardHTML(product)
+        })
+    }
